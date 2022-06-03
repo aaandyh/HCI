1a23251f4d121813e13330ebc7ba0c9ab6593836
aaandyh <61182312+aaandyh@users.noreply.github.com>
2022-06-03 20:34:20 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -1,15 +1,14 @@
 
-var rotationSpeed = 0.001;
+var rotationSpeed = 0.02;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/4;
 	console.log(myOtherBox.object3D.rotation);
 }
 
-setInterval(spin, 16);
+setInterval(spin, 32);
 
 
 myOtherBox.addEventListener('mouseenter', function(){
