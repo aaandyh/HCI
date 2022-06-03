febf2128de719f67123f610e46d268afeb1bdbd3
aaandyh <61182312+aaandyh@users.noreply.github.com>
2022-06-03 20:35:59 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -1,17 +1,12 @@
-
-var rotationSpeed = 0.02;
+// changing individual properties with code and using setInterval
+var rotationSpeed = 0.1;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/4;
-	console.log(myOtherBox.object3D.rotation);
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.y -= rotationSpeed;
+
+    console.log(myOtherBox.object3D.rotation.y);
 }
 
-setInterval(spin, 32);
-
-
-myOtherBox.addEventListener('mouseenter', function(){
-rotationSpeed = 0.01;
-console.log('mouse enter');
-});
+setInterval(spin, 32); //equivalent to 60 fps
